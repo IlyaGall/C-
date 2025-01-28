c21730b9a806255fc00c91f755562b1fd62b440a
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-01-28 14:35:41 +0300

    c#

---
+++ 35 Введение в параллелизм в .NET. Отличия процесса, потока, домена и таска/code/Otus.Teaching.Concurrency.Queue.Scheduler/Schedulers/ProcessesScheduler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Otus.Teaching.Concurrency.Queue.Access;
+
+namespace Otus.Teaching.Concurrency.Queue.Scheduler.Schedulers
+{
+    public class ProcessesScheduler
+        : IScheduler
+    {
+        private const string HandlerProcessFileName = "Otus.Teaching.Concurrency.Queue.Handler.Process.exe";
+        private const string HandlerProcessDirectory = @"C:\GitProjects\otus.teaching.concurrency.queue\Otus.Teaching.Concurrency.Queue.Handler.Process\publish";
+        readonly List<Process> _queueHandlerProcesses = new List<Process>();
+
+        public void ProcessQueue()
+        {
+            var stopWatch = new Stopwatch();
+            
+            Console.WriteLine("Process scheduler...");
+            Console.WriteLine("Handling queue...");
+            stopWatch.Start();
+
+            _queueHandlerProcesses.Add(StartHandlerProcess(MessageQueueItemType.Email));
+            _queueHandlerProcesses.Add(StartHandlerProcess(MessageQueueItemType.Sms));
+            _queueHandlerProcesses.Add(StartHandlerProcess(MessageQueueItemType.Push));
+            
+            _queueHandlerProcesses.ForEach(x => x.WaitForExit());
+            
+            stopWatch.Stop();
+            
+            Console.WriteLine($"Handled queue in {stopWatch.Elapsed}...");
+        }
+
+        private Process StartHandlerProcess(MessageQueueItemType type)
+        {
+            var startInfo = new ProcessStartInfo()
+            {
+                ArgumentList = {type.ToString()},
+                FileName = GetPathToHandlerProcess(),
+            };
+            
+            var process = Process.Start(startInfo);
+
+            return process;
+        }
+
+        private string GetPathToHandlerProcess()
+        {
+            return Path.Combine(HandlerProcessDirectory, HandlerProcessFileName);
+        }
+    }
+}
---
+++ 35 Введение в параллелизм в .NET. Отличия процесса, потока, домена и таска/code/Otus.Teaching.Concurrency.Queue.Scheduler/Schedulers/ThreadPoolSchedulerItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Otus.Teaching.Concurrency.Queue.Access;
+
+namespace Otus.Teaching.Concurrency.Queue.Scheduler.Schedulers
+{
+    public class ThreadPoolSchedulerItem
+    {
+        public MessageQueueItemType Type { get; private set; }
+
+        public WaitHandle WaitHandle { get; private set; }
+
+        public ThreadPoolSchedulerItem(MessageQueueItemType type)
+        {
+            Type = type;
+            WaitHandle = new AutoResetEvent(false);
+        }
+    }
+}
